5dbfee17479b7c7e56736cd2d1cc29db2216473e
Doston <sheraliyevdostonhoja@gmail.com>
2021-05-23 18:56:49 +0300

    changed

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import './translation/i18n';
 import App from './App';
 
-
 ReactDOM.render(
-    <Suspense fallback={'loading...'}>
+    <Suspense fallback={''}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
